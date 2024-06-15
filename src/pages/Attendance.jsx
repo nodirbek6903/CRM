a8f2a2d51e26b56741c31bc542d9a070556853fc
nodirchik6903 <nu2913634@gmail.com>
2024-06-15 14:08:11 +0500

    table added

--- src/pages/Attendance.jsx
+++ src/pages/Attendance.jsx
@@ -1,4 +1,5 @@
 import { FaPlus } from "react-icons/fa6";
+import { PiDotsThreeOutlineVerticalThin } from "react-icons/pi";
 import "./Attendance.css";
 
 export default function Attendance() {
@@ -27,17 +28,205 @@ export default function Attendance() {
         <div className="groups-table-container">
           <div className="table-description">
             <div className="groups-teacher">
-              <span className="teacher-about">Frontend 1 Guruhi Dars beruvchi: Ismoil aka</span>
+              <span className="teacher-about">
+                Frontend 1 Guruhi Dars beruvchi: Ismoil aka
+              </span>
               <button className="new-groups-btn">
                 <FaPlus /> Keyingisi yaratish
               </button>
             </div>
             <div className="davomat-dates">
-              <input type="date" className="start-date date-input" value="2021-11-08" />
+              <input
+                type="date"
+                className="start-date date-input"
+                defaultValue="2021-11-08"
+              />
               <span>To</span>
-              <input type="date" className="end-date date-input" value="2021-11-08" />
+              <input
+                type="date"
+                className="end-date date-input"
+                defaultValue="2021-11-08"
+              />
             </div>
           </div>
+          <table className="davomat-table">
+            <thead>
+              <tr>
+                <th className="th-familiya-ism">
+                  <PiDotsThreeOutlineVerticalThin className="table-dots-icon" /> Ism Familiya
+                </th>
+                <th>2021-08-11</th>
+                <th>2021-08-11</th>
+                <th>2021-08-11</th>
+                <th>2021-08-11</th>
+                <th>2021-08-11</th>
+                <th>2021-08-11</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="table-items">
+                <td className="table-item td-familiya-ism">
+                  <PiDotsThreeOutlineVerticalThin className="table-dots-icon" /> Abdurahmon Madaminov
+                </td>
+                <td className="table-item">
+                  Keldi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Kelmadi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Kelmadi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Keldi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Kelmadi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Keldi <input type="checkbox" className="davomat-change" />
+                </td>
+              </tr>
+              <tr className="table-items">
+                <td className="table-item td-familiya-ism">
+                  <PiDotsThreeOutlineVerticalThin className="table-dots-icon" /> Abdurahmon Madaminov
+                </td>
+                <td className="table-item">
+                  Keldi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Kelmadi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Kelmadi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Keldi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Kelmadi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Keldi <input type="checkbox" className="davomat-change" />
+                </td>
+              </tr>
+              <tr className="table-items">
+                <td className="table-item td-familiya-ism">
+                  <PiDotsThreeOutlineVerticalThin className="table-dots-icon" /> Abdurahmon Madaminov
+                </td>
+                <td className="table-item">
+                  Keldi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Kelmadi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Kelmadi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Keldi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Kelmadi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Keldi <input type="checkbox" className="davomat-change" />
+                </td>
+              </tr>
+              <tr className="table-items">
+                <td className="table-item td-familiya-ism">
+                  <PiDotsThreeOutlineVerticalThin className="table-dots-icon" /> Abdurahmon Madaminov
+                </td>
+                <td className="table-item">
+                  Keldi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Kelmadi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Kelmadi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Keldi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Kelmadi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Keldi <input type="checkbox" className="davomat-change" />
+                </td>
+              </tr>
+              <tr className="table-items">
+                <td className="table-item td-familiya-ism">
+                  <PiDotsThreeOutlineVerticalThin className="table-dots-icon" /> Abdurahmon Madaminov
+                </td>
+                <td className="table-item">
+                  Keldi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Kelmadi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Kelmadi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Keldi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Kelmadi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Keldi <input type="checkbox" className="davomat-change" />
+                </td>
+              </tr>
+              <tr className="table-items">
+                <td className="table-item td-familiya-ism">
+                  <PiDotsThreeOutlineVerticalThin className="table-dots-icon" /> Abdurahmon Madaminov
+                </td>
+                <td className="table-item">
+                  Keldi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Kelmadi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Kelmadi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Keldi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Kelmadi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Keldi <input type="checkbox" className="davomat-change" />
+                </td>
+              </tr>
+              <tr className="table-items">
+                <td className="table-item td-familiya-ism">
+                  <PiDotsThreeOutlineVerticalThin className="table-dots-icon" /> Abdurahmon Madaminov
+                </td>
+                <td className="table-item">
+                  Keldi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Kelmadi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Kelmadi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Keldi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Kelmadi <input type="checkbox" className="davomat-change" />
+                </td>
+                <td className="table-item">
+                  Keldi <input type="checkbox" className="davomat-change" />
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
